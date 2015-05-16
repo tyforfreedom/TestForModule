927e9c5a1db9299fb34c9151a5296b76b8774ae4
TimHongHong <tim.hong@zuolin.com>
2015-05-16 16:05:05 +0800

    hello

---
+++ TestForModule/TestForModule/TestForModule.h
@@ -0,0 +1,19 @@
+//
+//  TestForModule.h
+//  TestForModule
+//
+//  Created by Tim on 16/5/15.
+//  Copyright (c) 2015 TimHong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestForModule.
+FOUNDATION_EXPORT double TestForModuleVersionNumber;
+
+//! Project version string for TestForModule.
+FOUNDATION_EXPORT const unsigned char TestForModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestForModule/PublicHeader.h>
+
+
---
+++ TestForModule/TestForModule/src/TimTestHeader.h
@@ -0,0 +1,13 @@
+//
+//  TimTestHeader.h
+//  TestForModule
+//
+//  Created by Tim on 16/5/15.
+//  Copyright (c) 2015 TimHong. All rights reserved.
+//
+
+#ifndef TestForModule_TimTestHeader_h
+#define TestForModule_TimTestHeader_h
+
+//Hello
+#endif
